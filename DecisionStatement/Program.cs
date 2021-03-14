495f55ed49bb3e3ad832c2a87224889d81f6a062
Anshurani94 <44113010+Anshurani94@users.noreply.github.com>
2021-03-14 23:36:19 +0530

    Add files via upload

---
+++ DecisionStatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DecisionStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Choose a door; 1, 2 or 3");
+            string userValue = Console.ReadLine();
+            
+            string message = "";
+
+            if (userValue == "1")
+                message = "You won a cat";
+            else if (userValue == "2")
+                message = "You won a boat";
+            else if (userValue == "3")
+                message = "You won a car";
+            else
+            {
+                message = "Sorry, we didn't understand";
+                message = message + "You lose";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+    }
+}
