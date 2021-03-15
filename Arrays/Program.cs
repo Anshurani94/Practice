d5f2ba3367d6ced4cf4394e706f58c883c88a518
Anshurani94 <44113010+Anshurani94@users.noreply.github.com>
2021-03-15 19:00:06 +0530

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int[] numbers = new int[5];
+
+             numbers[0] = 4;
+             numbers[1] = 8;
+             numbers[2] = 15;
+             numbers[3] = 16;
+             numbers[4] = 23;
+
+             Console.WriteLine(numbers[1]);
+             Console.WriteLine(numbers.Length);
+             Console.ReadLine();
+             */
+            string[] names = new string[] { "anshu", "garima", "priya", "abhi", "anu", "angel" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+            
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ CallingMethod/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CallingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The New Game");
+
+            Console.Write("What is your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name: ");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your city name: ");
+            string city = Console.ReadLine();
+
+            char[] firstNameArray = firstName.ToCharArray();
+            Array.Reverse(firstNameArray);
+
+            char[] lastNameArray = lastName.ToCharArray();
+            Array.Reverse(lastNameArray);
+
+            char[] cityArray = city.ToCharArray();
+            Array.Reverse(cityArray);
+
+            string result = "";
+
+            foreach (char item in firstNameArray)
+            {
+                result += item;
+            }
+
+            foreach (char item in lastNameArray)
+            {
+                result += item;
+            }
+
+            foreach (char item in cityArray)
+            {
+                result += item;
+            }
+
+            Console.WriteLine("Results: " + result);
+            
+            Console.ReadLine();
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
