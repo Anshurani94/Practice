c151fd1b9ee6be24fcd84d2f90e4fdce6921478c
Anshurani94 <44113010+Anshurani94@users.noreply.github.com>
2021-03-15 22:41:13 +0530

    Add files via upload

---
+++ DateAndTime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+            //DateTime myBirthday = new DateTime(1994, 01, 10);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+
+            DateTime myBirthday = DateTime.Parse("10/01/1994");
+            TimeSpan myage = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myage.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringsProgram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StringsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myString = "My \"so-called\" life";
+            //string myString = " What if I need a\nnew line?";
+            //string myString = "Go to your c:\\ drive";
+            //string myString = @"Go to your c:\ drive";
+
+            //string myString = string.Format("{0} = {1}", "First", "Second");
+            //string myString = string.Format("{0:C}", 120.90);
+           
+            string myString = "  That summer we took threes across the board  ";
+
+            //myString = myString.Substring(6, 14);
+            //myString = myString.ToUpper();
+
+            /*myString = String.Format("Length before: {0} -- Length after: {1}",
+                myString.Length,
+                myString.Trim().Length);
+            */
+            
+            for (int i = 0; i < 100; i++)
+            {
+                myString += "--" + i.ToString();
+                
+            }
+
+            
+
+
+
+
+
+            Console.WriteLine(myString);
+            Console.ReadLine();
+        }
+    }
+}
