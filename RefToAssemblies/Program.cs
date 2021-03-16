412b3e41d4c01b993abbcdbdbd11656b3537fa6f
Anshurani94 <44113010+Anshurani94@users.noreply.github.com>
2021-03-16 20:49:25 +0530

    Add files via upload

---
+++ RefToAssemblies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefToAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
